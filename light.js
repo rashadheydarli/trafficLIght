68879dba206f74fb23923de261b976cda5a8cab5
rashadheydarli <rashadahe@code.edu.az>
2023-04-26 22:21:24 +0400

    second commit

--- light.js
+++ light.js
@@ -4,15 +4,16 @@ function startLight() {
     const green = document.getElementsByClassName("red")[0];
     const yellow = document.getElementsByClassName("yellow")[0];
     const red = document.getElementsByClassName("green")[0];
-
-    setTimeout(function () {
-        red.style.opacity = 1;
-        yellow.style.opacity = .2;
-        green.style.opacity = .2;
-    },2000);
+    
+    green.style.opacity=1;
     setTimeout(function () {
         red.style.opacity = .2;
         yellow.style.opacity = 1;
+        green.style.opacity = .2;
+    },2000);
+    setTimeout(function () {
+        red.style.opacity = 1;
+        yellow.style.opacity = .2;
         green.style.opacity = .2;
     },4000);
     setTimeout(function () {
