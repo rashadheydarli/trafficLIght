27a4db9d1c29337dc84dd239b0ec5ebd527e3121
rashadheydarli <rashadahe@code.edu.az>
2023-04-26 21:50:48 +0400

    traffic light added

---
+++ light.js
@@ -0,0 +1,30 @@
+
+function startLight() {
+
+    const green = document.getElementsByClassName("red")[0];
+    const yellow = document.getElementsByClassName("yellow")[0];
+    const red = document.getElementsByClassName("green")[0];
+
+    setTimeout(function () {
+        red.style.opacity = 1;
+        yellow.style.opacity = .2;
+        green.style.opacity = .2;
+    },2000);
+    setTimeout(function () {
+        red.style.opacity = .2;
+        yellow.style.opacity = 1;
+        green.style.opacity = .2;
+    },4000);
+    setTimeout(function () {
+        red.style.opacity = .2;
+        yellow.style.opacity = .2;
+        green.style.opacity = 1;
+    },6000);
+
+}
+const timer = setInterval(function() {
+    startLight();
+},6000);
+
+startLight();
+
